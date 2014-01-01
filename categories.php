0dab2f87f5ed518b2d9610ceec8fb35b9518a177
hikikomorime <michal@skweres.net>
2014-01-01 22:15:31 +0100

    Create categories.php

---
+++ categories.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+	'category' => 'Kategoria',
+	'categories' => 'Kategorie',
+
+	'create_category' => 'Utwórz nową kategorię',
+	'edit_category' => 'Edytujesz &ldquo;%s&rdquo;',
+
+	// form fields
+	'title' => 'Tytuł',
+	'title_explain' => 'Tytuł nowej kategorii.',
+	'title_missing' => 'Prosze wpisać tytuł',
+
+	'slug' => 'Skrót',
+	'slug_explain' => 'Skrócony adres kategorii.',
+
+	'description' => 'Opis',
+	'description_explain' => 'Opisz nową kategorię.',
+
+	// messages
+	'created' => 'Nowa kategoria została dodana.',
+	'updated' => 'Kategoria została zaktualizowana.',
+	'deleted' => 'Kategoria została usunięta.',
+	'delete_error' => 'Musisz wybrać co najmniej jedną kategorię.',
+
+);
