e146c977cfe94ea2c832ed414d9a7814739f1753
hikikomorime <michal@skweres.net>
2014-01-01 22:15:47 +0100

    Create comments.php

---
+++ comments.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+
+	'comments' => 'Komentarze',
+	'nocomments_desc' => 'Brak komentarzy.',
+	'editing_comment' => 'Edytuj komentarz',
+	'view_comment' => 'Zobacz komentarz',
+
+	// form fields
+	'name' => 'Nick lub imię',
+	'name_explain' => 'Nick lub imię komentującego',
+	'name_missing' => 'Proszę podać nick lub imię',
+
+	'email' => 'Adres e-mail',
+	'email_explain' => 'Adres e-mail komentującego',
+	'email_missing' => 'Proszę podać właściwy adres e-mail', // frontend message (appears on your site!)
+
+	'text' => 'Komentarz',
+	'text_explain' => '',
+	'text_missing' => 'Proszę wpisać treść komentarza', // frontend message (appears on your site!)
+
+	'status' => 'Status',
+	'status_explain' => '',
+
+	// messages
+	'created' => 'Komentarz został zapisany', // frontend message (appears on your site!)
+	'updated' => 'Komentarz został zaktualizowany',
+	'deleted' => 'Komentarz został usunięty',
+
+	// email notification
+	'notify_subject' => 'Pojawił się nowy komentarz',
+	'nofity_heading' => 'Na Twojej stronie pojawił się nowy komentarz.'
+
+);
