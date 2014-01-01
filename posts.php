5200bd7d0820b46ffff525c4f87639617e040957
hikikomorime <michal@skweres.net>
2014-01-01 22:17:04 +0100

    Create posts.php

---
+++ posts.php
@@ -0,0 +1,47 @@
+<?php
+
+return array(
+
+	'posts' => 'Wpisy',
+
+	'create_post' => 'Utwórz nowy wpis',
+	'noposts_desc' => 'Nie masz jeszcze żadnych wpisów!',
+
+	// form fields
+	'title' => 'Tytuł wpisu',
+	'title_explain' => '',
+	'title_missing' => 'Prosze podać tytuł',
+
+	'content' => 'Treść wpisu',
+	'content_explain' => 'Po prostu zacznij tutaj pisać :)',
+
+	'slug' => 'Skrócony adres wpisu',
+	'slug_explain' => 'Skrót linku. Nie rób spacji ani znaków specjalnych.',
+	'slug_missing' => 'Pamiętaj,aby nie używać znaków specjalnych',
+	'slug_duplicate' => 'Taki skrót już istnieje',
+	'slug_invalid' => 'Skrót musi zawierać litery',
+
+	'description' => 'Opis',
+	'description_explain' => '',
+
+	'status' => 'Status',
+	'status_explain' => '',
+
+	'category' => 'Kategoria',
+	'category_explain' => '',
+
+	'allow_comments' => 'Zezwól na komentarze',
+	'allow_comments_explain' => '',
+
+	'custom_css' => 'Dodatkowy styl CSS',
+	'custom_css_explain' => '',
+
+	'custom_js' => 'Dodatkowy skrypt JS',
+	'custom_js_explain' => '',
+
+	// messages
+	'updated' => 'Twój wpis został zaktualizowany',
+	'created' => 'Twój wpis został zapisany',
+	'deleted' => 'Twój wpis został usunięty'
+
+);
