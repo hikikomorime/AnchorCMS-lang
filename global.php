9d62d0a4440a4f1d0da1c3403fc18fbb1f9989b3
hikikomorime <michal@skweres.net>
2014-01-01 22:16:13 +0100

    Create global.php

---
+++ global.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Global is to replace common.php
+ *
+ * This global file will provide the majority of the translations in anchor
+ * to remove duplicate translations in section specific language files.
+ */
+return array(
+
+	// words
+	'save' => 'Zapisz',
+	'delete' => 'Usuń',
+	'update' => 'Zaktualizuj',
+	'edit' => 'Edytuj',
+	'editing' => 'Edycja',
+	'create' => 'Utwórz',
+	'created' => 'Utworzono',
+	'submit' => 'Prześlij',
+	'close' => 'Zamknij',
+	'status' => 'Status',
+	'manage' => 'Zarządzaj',
+	'reset' => 'Resetuj',
+	'all' => 'Wszystkie',
+
+	// pagination
+	'next' => 'Dalej',
+	'previous' => 'Wstecz',
+	'first' => 'Początek',
+	'last' => 'Koniec',
+
+	// statuses
+	'draft' => 'Szkic',
+	'archived' => 'Archiwum',
+	'published' => 'Opublikowane',
+	'pending' => 'Oczekujące',
+	'approved' => 'Przyjęte',
+	'spam' => 'Spam',
+
+	'inactive' => 'Nieaktywne',
+	'active' => 'Aktywne',
+
+	// roles
+	'administrator' => 'Administrator',
+	'editor' => 'Edytor',
+	'user' => 'Uzytkownik',
+
+	'log_in' => 'Zaloguj się',
+	'login' => 'Zaloguj się',
+	'log_out' => 'Wyloguj się',
+	'logout' => 'Wyloguj się',
+
+	// pharses
+	'visit_your_site' => 'Idź na stronę',
+	'powered_by_anchor' => 'Powered by Livepoint.pl <> (Anchor, version %s)',
+	'make_blogging_beautiful' => 'Simple, clean and traditional design.',
+
+	// intro
+	'welcome_to_anchor' => 'Witaj w panelu Livepoint',
+	'welcome_to_anchor_lets_go' => 'Witaj w panelu Livepoint - zaczynajmy!.',
+	'run_the_installer' => 'Uruchom instalację',
+
+	// upgrade
+	'upgrade' => 'Aktualizacja',
+	'good_news' => 'Dobre wieści!',
+	'new_version_available' => 'Pojawiła się nowa wersja systemu, z którego korzystasz.',
+	'download_now' => 'Pobierz teraz',
+	'upgrade_later' => 'Zaktualizuj później',
+
+	// debug profiler
+	'profile' => 'Profil',
+	'profile_memory_usage' => 'Razem pamięci w użyciu',
+
+	// messages
+	'confirm_delete' => 'Na pewno chcesz usunąć? Tego nie można cofnąć!'
+
+);
