36fb0d19a3f9e18f46cb06e8daff6347aa833a03
hikikomorime <michal@skweres.net>
2014-01-01 22:16:40 +0100

    Create metadata.php

---
+++ metadata.php
@@ -0,0 +1,44 @@
+<?php
+
+return array(
+
+	'metadata' => 'Dane Twojej strony',
+	'metadata_desc' => 'Zarządzaj danymi Twojej strony',
+
+	'comment_settings' => 'Komentarze',
+	'theme_settings' => 'Wygląd',
+
+	// form fields
+	'sitename' => 'Nazwa strony',
+	'sitename_explain' => '',
+	'sitename_missing' => 'Twoja strona musi mieć nazwę wiesz?',
+
+	'sitedescription' => 'Opis strony',
+	'sitedescription_explain' => '',
+	'sitedescription_missing' => 'Twoja strona poza nazwą musi miec jakiś opis',
+
+	'homepage' => 'Strona główna',
+	'homepage_explain' => '',
+
+	'postspage' => 'Strona wpisów',
+	'postspage_explain' => '',
+
+	'posts_per_page' => 'Ilość wpisów na stronę',
+	'posts_per_page_explain' => '',
+
+	'auto_publish_comments' => 'Czy automatycznie przyjmować komentarze?',
+	'auto_publish_comments_explain' => '',
+
+	'comment_notifications' => 'Chcesz być powiadamiany o nowych komentarzach?',
+	'comment_notifications_explain' => '',
+
+	'comment_moderation_keys' => 'Słowa uznawane za spam',
+	'comment_moderation_keys_explain' => 'Wpisz oddzielone przecinkami słowa, które automatycznie gdy wystąpią w komentarzach mają zakwalifikować go do spamu.',
+
+	'current_theme' => 'Aktualnie aktywny szablon',
+	'current_theme_explain' => '',
+
+	// messages
+	'updated' => 'Dane zostały zaktualizowane',
+
+);
