5a9a1f904886c8fdeae218efc66b7cfdd81efdfe
hikikomorime <michal@skweres.net>
2014-01-01 22:17:18 +0100

    Create users.php

---
+++ users.php
@@ -0,0 +1,67 @@
+<?php
+
+return array(
+
+	'users' => 'Konta',
+
+	'create_user' => 'Utwórz nowego użytkownika',
+	'add_user' => 'Dodaj użytkownika',
+	'editing_user' => 'Edytujesz profil %s&rsquo;',
+	'remembered' => 'Znam swoje hasło',
+	'forgotten_password' => 'Zapomniałem hasła',
+
+	// roles
+	'administrator' => 'Administrator',
+	'administrator_explain' => '',
+
+	'editor' => 'Edytor',
+	'editor_explain' => '',
+
+	'user' => 'Użytkownik',
+	'user_explain' => '',
+
+	// form fields
+	'real_name' => 'Imię i nazwisko',
+	'real_name_explain' => '',
+
+	'bio' => 'Bio',
+	'bio_explain' => '',
+
+	'status' => 'Status',
+	'status_explain' => '',
+
+	'role' => 'Rola',
+	'role_explain' => '',
+
+	'username' => 'Login',
+	'username_explain' => '',
+	'username_missing' => 'Login musi mieć co najmniej %s znaków',
+
+	'password' => 'Hasło',
+	'password_explain' => '',
+	'password_too_short' => 'Hasło musi mieć co najmniej %s znaków',
+
+	'new_password' => 'Nowe hasło',
+
+	'email' => 'E-mail',
+	'email_explain' => '',
+	'email_missing' => 'Prosze podać właściwy e-mail',
+	'email_not_found' => 'Nie znaleziono takiego konta.',
+
+	// messages
+	'updated' => 'Konto zostało zaktualizowane',
+	'created' => 'Konto zostało utworzone',
+	'deleted' => 'Konto zostało usunięte',
+	'delete_error' => 'Nie możesz usunąć swojego profilu',
+	'login_error' => 'Podano zły login lub hasło.',
+	'logout_notice' => 'Zostałeś wylogowany.',
+	'recovery_sent' => 'Wysłano e-mail potwierdzający zmianę hasła.',
+	'recovery_expired' => 'Token zmiany hasła wygasł. Spróbuj ponownie.',
+	'password_reset' => 'Twoje nowe hasło zostało przyjęte. Zaloguj się.',
+
+	// password recovery email
+	'recovery_subject' => 'Resetowanie hasła',
+	'recovery_message' => 'Odnotowano prośbę zmiany hasła.' .
+		'Aby kontynuować, kliknij poniższy link.' . PHP_EOL . '%s',
+
+);
