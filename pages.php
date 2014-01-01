fae564196f969bf45097ec442d34f1aa1591e1eb
hikikomorime <michal@skweres.net>
2014-01-01 22:16:52 +0100

    Create pages.php

---
+++ pages.php
@@ -0,0 +1,45 @@
+<?php
+
+return array(
+
+	'pages' => 'Strony',
+
+	'create_page' => 'Utwórz nową stronę',
+	'nopages_desc' => 'Nie masz jeszcze żadnych stron.',
+	'redirect' => 'Przekierowanie',
+
+	// form fields
+	'redirect_url' => 'Adres przekierowania',
+	'redirect_missing' => 'Wprowadź prawidłowy adres url przekierowania',
+
+	'title' => 'Tytuł strony',
+	'title_explain' => '',
+	'title_missing' => 'Proszę wpisać tytuł strony',
+
+	'content' => 'Treść',
+	'content_explain' => 'Treść znajdująca się na stronie. Używamy składni Markdown.',
+
+	'show_in_menu' => 'Pokazywać tę stronę w menu głównym?',
+	'show_in_menu_explain' => '',
+
+	'name' => 'Nazwa',
+	'name_explain' => '',
+
+	'slug' => 'Skrócony adres strony',
+	'slug_explain' => 'Skrót linku. Nie rób spacji ani znaków specjalnych',
+	'slug_missing' => 'Pamiętaj,aby nie używać znaków specjalnych',
+	'slug_duplicate' => 'Taki skrót już istnieje',
+	'slug_invalid' => 'Skrót musi zawierać litery',
+
+	'status' => 'Status',
+	'status_explain' => '',
+
+	'parent' => 'Strona nadrzędna',
+	'parent_explain' => '',
+
+	// messages
+	'updated' => 'Twoja strona została zaktualizowana',
+	'created' => 'Twoja strona została utworzona',
+	'deleted' => 'Twoja strona została usunięta'
+
+);
